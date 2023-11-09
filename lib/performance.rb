7f03a51b4bb5d181499767e28c2053fc1e1ea1d9
shket344 <kalimador@tut.by>
2023-11-09 16:24:42 +0300

    Count incomes for each drug

--- lib/performance.rb
+++ lib/performance.rb
@@ -1,16 +1,18 @@
 require_relative 'application'
 
 class Performance < Application
-  attr_reader :reader, :file_path, :data_collector
+  attr_reader :reader, :file_path, :data_collector, :drug_counter
 
-  def initialize(reader, file_path, data_collector)
+  def initialize(reader, file_path, data_collector, drug_counter)
     @reader = reader
     @file_path = file_path
     @data_collector = data_collector
+    @drug_counter = drug_counter
   end
 
   def call
     read_file = reader.call(file_path)
-    data_collector.call(read_file)
+    collected_data = data_collector.call(read_file)
+    drug_counter.call(collected_data)
   end
 end
