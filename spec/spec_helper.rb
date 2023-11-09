739449524bf6c092ea008b59b5e3103df5de5cde
shket344 <kalimador@tut.by>
2023-11-09 20:55:36 +0300

    Test coverage for Reader

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
+require 'simplecov'
+
+SimpleCov.start
+
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
@@ -11,3 +15,5 @@ RSpec.configure do |config|
 
   config.shared_context_metadata_behavior = :apply_to_host_groups
 end
+
+RSpec::Expectations.configuration.on_potential_false_positives = :nothing
