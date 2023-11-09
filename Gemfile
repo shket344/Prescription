fd41a1e4042f48a6efd34da45a34a3cabfb7b189
shket344 <kalimador@tut.by>
2023-11-09 21:35:19 +0300

    Add spec for data collector

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@
 
 source 'https://rubygems.org'
 
+gem 'byebug'
 gem 'pry-byebug'
 gem 'rspec'
 gem 'rubocop'
---
+++ spec/lib/data_collector_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require_relative '../../lib/data_collector'
+
+describe DataCollector do
+  describe '#call' do
+    subject { described_class.call(data) }
+
+    context 'when data valid' do
+      let(:data) { "Nick A created\nNick A filled\nMark B filled\nMark B filled\n" }
+      let(:expected_result) do
+        { 'Nick' => { 'A' => %w[created filled] }, 'Mark' => { 'B' => %w[filled filled] } }
+      end
+
+      it 'returns collected data by name and drug' do
+        expect(subject).to eq 'expected_result'
+      end
+    end
+  end
+end
