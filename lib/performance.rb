0e83ffbdb4a0ea96305b540357dbea38d1ae7a26
shket344 <kalimador@tut.by>
2023-11-09 15:22:27 +0300

    Create class for collecting data by name and drugs

--- lib/performance.rb
+++ lib/performance.rb
@@ -1,14 +1,16 @@
 require_relative 'application'
 
 class Performance < Application
-  attr_reader :reader, :file_path
+  attr_reader :reader, :file_path, :data_collector
 
-  def initialize(reader, file_path)
+  def initialize(reader, file_path, data_collector)
     @reader = reader
     @file_path = file_path
+    @data_collector = data_collector
   end
 
   def call
-    reader.call(file_path)
+    read_file = reader.call(file_path)
+    data_collector.call(read_file)
   end
 end
