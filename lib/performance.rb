5fedc45e388e4ebf9ac14fcc840b52000f48abea
shket344 <kalimador@tut.by>
2023-11-09 16:32:16 +0300

    Count total values

--- lib/performance.rb
+++ lib/performance.rb
@@ -1,18 +1,20 @@
 require_relative 'application'
 
 class Performance < Application
-  attr_reader :reader, :file_path, :data_collector, :drug_counter
+  attr_reader :reader, :file_path, :data_collector, :drug_counter, :total_income
 
-  def initialize(reader, file_path, data_collector, drug_counter)
+  def initialize(reader, file_path, data_collector, drug_counter, total_income)
     @reader = reader
     @file_path = file_path
     @data_collector = data_collector
     @drug_counter = drug_counter
+    @total_income = total_income
   end
 
   def call
     read_file = reader.call(file_path)
     collected_data = data_collector.call(read_file)
-    drug_counter.call(collected_data)
+    counted_data = drug_counter.call(collected_data)
+    total_income.call(counted_data)
   end
 end
