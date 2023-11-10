1a245215335035492befde144c0181d1e7f89e53
Kirill Gleb <70264606+Zandelok@users.noreply.github.com>
2023-11-10 14:05:09 +0300

    Update spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,9 +10,7 @@ RSpec.configure do |config|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
 
-  config.exit_code do |exit_code|
-    exit_code.number_failures
-  end
+  config.failure_exit_code = 1
 
   config.mock_with :rspec do |mocks|
     mocks.verify_partial_doubles = true
